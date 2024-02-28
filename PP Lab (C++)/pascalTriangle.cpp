ce067b5486910363d31973464ecdd19cef613c0f
MrSaltyFish <anveshkhode7588@gmail.com>
2024-02-28 20:53:03 +0530

    CLL - insertAtPosition, --- DLL - insertAtBegin, insertAtEnd, printForwards, printBackwards

--- PP Lab (C++)/pascalTriangle.cpp
+++ PP Lab (C++)/pascalTriangle.cpp
@@ -3,29 +3,40 @@
 
 #include <iostream>
 using namespace std;
-void generatePascalsTriangle(int numRows) {
+
+void generatePascalsTriangle(int numRows)
+{
     int pascal_triangle[numRows][numRows];
-    for (int i = 0; i < numRows; ++i) {
-        for (int j = 0; j <= i; ++j) {
-            if (j == 0 || j == i) {
+    for (int i = 0; i < numRows; ++i)
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            if (j == 0 || j == i)
+            {
                 pascal_triangle[i][j] = 1;
-            } else {
+            }
+            else
+            {
                 pascal_triangle[i][j] = pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j];
             }
         }
     }
-    for (int i = 0; i < numRows; ++i) {
-        for (int space = 0; space < numRows - i - 1; ++space) {
+    for (int i = 0; i < numRows; ++i)
+    {
+        for (int space = 0; space < numRows - i - 1; ++space)
+        {
             cout << "  ";
         }
 
-        for (int j = 0; j <= i; ++j) {
+        for (int j = 0; j <= i; ++j)
+        {
             cout << pascal_triangle[i][j] << "   ";
         }
-        cout <<endl;
+        cout << endl;
     }
 }
-int main() {
+int main()
+{
     int numRows;
     cout << "Enter the number of rows for Pascal's Triangle: ";
     cin >> numRows;
